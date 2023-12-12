44d0db66ca9db5ca69e37874d3e62eb069e964a0
xqkeji <xqkeji@foxmail.com>
2023-12-12 19:37:59 +0800

    更新菜单格式

--- src/config/menu.php
+++ src/config/menu.php
@@ -1,42 +1,45 @@
 <?php
 return [
-	'title'=>'客户关系管理',
-	'children'=>[
-		[
-			'url'=>'customer_type/admin',
-			'title'=>'客户类别管理',
-			'icon'=>'bi bi-columns',
-		],
-		[
-			'url'=>'customer_source/admin',
-			'title'=>'客户来源管理',
-			'icon'=>'bi bi-view-stacked',
-		],
-		[
-			'url'=>'customer/admin',
-			'title'=>'客户信息管理',
-			'icon'=>'bi bi-info-square',
-		],
-		[
-			'url'=>'credit_level/admin',
-			'title'=>'信用等级管理',
-			'icon'=>'bi bi-water',
-		],
-		[
-			'url'=>'credit/admin',
-			'title'=>'客户信用管理',
-			'icon'=>'bi bi-credit-card-2-front',
-		],
-		[
-			'url'=>'order/admin',
-			'title'=>'客户订单管理',
-			'icon'=>'bi bi-cart',
-		],
-		[
-			'url'=>'track/admin',
-			'title'=>'客户跟踪管理',
-			'icon'=>'bi bi-sign-railroad',
-		],
-	]
+	'admin'=>[
+		'title'=>'客户关系管理',
+		'children'=>[
+			[
+				'url'=>'customer_type/admin',
+				'title'=>'客户类别管理',
+				'icon'=>'bi bi-columns',
+			],
+			[
+				'url'=>'customer_source/admin',
+				'title'=>'客户来源管理',
+				'icon'=>'bi bi-view-stacked',
+			],
+			[
+				'url'=>'customer/admin',
+				'title'=>'客户信息管理',
+				'icon'=>'bi bi-info-square',
+			],
+			[
+				'url'=>'credit_level/admin',
+				'title'=>'信用等级管理',
+				'icon'=>'bi bi-water',
+			],
+			[
+				'url'=>'credit/admin',
+				'title'=>'客户信用管理',
+				'icon'=>'bi bi-credit-card-2-front',
+			],
+			[
+				'url'=>'order/admin',
+				'title'=>'客户订单管理',
+				'icon'=>'bi bi-cart',
+			],
+			[
+				'url'=>'track/admin',
+				'title'=>'客户跟踪管理',
+				'icon'=>'bi bi-sign-railroad',
+			],
+		]
+	],
+	
 	
 ];
